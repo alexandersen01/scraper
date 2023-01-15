ee00a49b8336ef05c8f7456e58f5054f48fb7f31
alexandersen01 <116839910+alexandersen01@users.noreply.github.com>
2023-01-15 16:23:42 +0100

    Update bolig_til_salg.py
    looking kinda sexy ngl

--- bolig_til_salg.py
+++ bolig_til_salg.py
@@ -3,11 +3,14 @@ import pandas as pd
 import time
 import random
 from bs4 import BeautifulSoup
+from geopy.geocoders import Nominatim
+import folium
+import folium.plugins
 
 url_lst = []
 '''creates a list of urls to scrape from'''
 
-for i in range (1, 5):
+for i in range (1, 15):
     response = requests.get(f'https://www.finn.no/realestate/homes/search.html?page={i}&sort=PUBLISHED_DESC')
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -20,12 +23,12 @@ for i in range (1, 5):
             pass
     time.sleep(random.randint(1000, 2000)/1000)
 
-'''create empty dataframe with specified columns '''
-
-df = pd.DataFrame(columns=['address', 'price', 'area', 'rooms', 'year', 'type', 'url', 'price_per_m2'])
-
 counter = 0
 
+'''create empty dataframe with specified columns '''
+
+df = pd.DataFrame(columns=['address', 'price', 'area', 'rooms', 'year', 'type', 'url', 'price_per_m2', 'lat', 'lon', 'date'])
+adr_lst = []
 for url_i in range(len(url_lst)):
     response = requests.get(url_lst[url_i])
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -34,6 +37,15 @@ for url_i in range(len(url_lst)):
         adr = soup.find_all('span', attrs={'class': 'pl-4'})[0].text
     except:
         adr = 'NA'
+
+    try:
+        geolocator = Nominatim(user_agent="myGeocoder")
+        location = geolocator.geocode(adr)
+        lat = location.latitude
+        lon = location.longitude
+    except:
+        lat = 'NA'
+        lon = 'NA'
 
     try:
         price = soup.find_all('span', attrs={'class': 'text-28 font-bold'})[0].text.replace(' ', '').replace('kr', '')
@@ -71,11 +83,14 @@ for url_i in range(len(url_lst)):
     
     url = url_lst[url_i]
 
+    #add todays date
+    date = time.strftime("%d/%m/%Y")
 
-    df = df.append({'address': adr, 'price' : price, 'area' : area, 'rooms' : rooms, 'year' : year, 'type' : type, 'url' : url, 'price_per_m2' : price_per_m2}, ignore_index=True)
+
+    df = df.append({'address': adr, 'price' : price, 'area' : area, 'rooms' : rooms, 'year' : year, 'type' : type, 'url' : url, 'price_per_m2' : price_per_m2, 'lat' : lat, 'lon' : lon, 'date' : date}, ignore_index=True)
 
     time.sleep(random.randint(500, 1000)/1000)
-    
+
     counter += 1
     print(counter)
 
@@ -130,18 +145,33 @@ df['price'] = df['price'].str.replace(' ', '')
 df = df[~df['city'].str.contains('Video')]
 df = df[~df['city'].str.contains('Visning')]
 
-# Save df as csv with today's date:
+# Save df as excel with today's date:
 df.to_excel('price_data_norway_' + str(time.strftime("%Y%m%d")) + '.xlsx', index=False)
 
+'''remove rows with NA in lat and lon'''
+df = df[df['lat'] != 'NA']
+df = df[df['lon'] != 'NA']
 
+'''display locations as circle map with folium'''
+map1 = folium.Map(location = [59.940077, 10.764767], tiles = 'CartoDB dark_matter', zoom_start = 7)
+df.apply(lambda row : folium.CircleMarker(location = [row['lat'], row['lon']]).add_to(map1), axis = 1)
+cluster = folium.plugins.MarkerCluster().add_to(map1)
+
+'''add info to popups on map'''
+
+for (index, row) in df.iterrows():
+    iframe = folium.IFrame('Address: ' + str(row['address']) + '<br>' + 'Price: ' + str(row['price']) + ' kr' + '<br>' + 'Price per m2: ' + str(row['price_per_m2']) + ' kr' + '<br>' + 'Area: ' + str(row['area']) + '<br>' + 'Rooms: ' + str(row['rooms']) + '<br>' + 'Year: ' + str(row['year']) + '<br>' + 'Type: ' + str(row['type']) + '<br>' + 'URL: ' + str(row['url']) + '<br>' + 'Date fetched: ' + str(row['date']))
+    popup = folium.Popup(iframe, min_width = 300, max_width = 400, min_height = 300, max_height = 400)
+    folium.Marker([row['lat'], row['lon']], popup = popup).add_to(cluster)
+
+#save map as html file with today's date
+map1.save('map_' + str(time.strftime("%Y%m%d")) + '.html')
 
 print(df)
 
-# To do: 
-# 1. Add date column
-# 2. Add column with number of rooms - done
+# To do:
+
 # 3. Add column with number of bathrooms
-# 4. add, update and print counter after every iteration of ads fetched
 
 # Overall goal: 
 # Calculate estimated gross yield for each property 
